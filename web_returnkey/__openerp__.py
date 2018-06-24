2d8908db372f51065e818e9e4b0e5cba4675eb6b
Ahmed Tarek <ahmed.tarek.mohamed90@gmail.com>
2018-06-24 14:20:51 +0200

    enter key

---
+++ web_returnkey/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Replace (tab key) with (Enter key)',
+    'author': 'Ahmed Tarek',
+    'description': """
+Web UI: "Return" Key
+====================
+Replace (tab key) with (Enter key) for field navigation.
+--------------------------------------------------------
+
+""",
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'custom_keyboard_shortcuts.xml',
+    ],
+}
